367d9953602ac89028c39161258a77b8cf229dbf
Alec Maier <Maier.144@osu.edu>
2019-02-20 21:44:22 -0500

    Printing dictionary of nicknames mapped to user ids

--- choreBot.py
+++ choreBot.py
@@ -7,9 +7,18 @@ response_messages = requests.get('https://api.groupme.com/v3/groups/48409659/mes
 
 group = requests.get('https://api.groupme.com/v3/groups/48409659', params = request_params).json()['response']['members']
 
+member_dict = {}
+
+daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+for member in group:
+    member_dict[member['nickname']] = member['user_id']
+
+print(member_dict)
+
 userIds = []
 
-print(json.dumps(group, indent=4))
+#print(json.dumps(group, indent=4))
 
 # client = Client.from_token('7bC53ZymUUULq74uIlnYHJ3WExGkGJMevOXGitY3')
 
